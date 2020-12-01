40468362a01d8d76985ef5d45028a4893c8019e8
Simon Galvis <simongalvis9@gmail.com>
2020-11-30 23:42:55 -0700

    Set up initial repo structure

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import {Route, Link } from 'react-router-dom';
+//import config from '../config';
+import './App.css';
+import NavBar from '../NavBar/NavBar'
+
+
+
+class App extends React.Component {
+
+  render(){
+  return (
+    <div className="App">
+      <NavBar/>
+      
+
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css'
+
+
+class NavBar extends React.Component{
+
+
+
+    render(){
+        return(
+            <h3>BreakawayNav</h3>
+        )
+    }
+}
+
+
+export default NavBar;
