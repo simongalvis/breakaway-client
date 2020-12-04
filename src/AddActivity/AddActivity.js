21640558013517e76280552a830cfcbbd3ef46e5
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 19:27:34 -0500

    Adjust form heights

---
+++ src/AddActivity/AddActivity.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import DashboardNavBar from '../DashboardNavBar/DashboardNavBar';
+import './AddActivity.css'
+
+
+class AddActivity extends React.Component{
+
+
+
+    render(){
+        return(
+            <div className='AddActivity'>
+                <DashboardNavBar/>
+                <h1>Form here</h1>
+                    
+                    
+                    
+                
+                
+            </div>
+        
+        )
+    }
+}
+
+export default AddActivity;
--- src/SignupPage/SignupPage.js
+++ src/SignupPage/SignupPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../SignupPage/SignupPage.css';
 import NavBar from '../NavBar/NavBar.js';
+import { Link } from 'react-router-dom';
 
 
 
@@ -14,7 +15,10 @@ class SignupPage extends React.Component{
                 <form>
                     <input  type='text' id='signupusername' name='signupusername' placeholder='Username'/>
                     <input  type='text' id='signuppassword' name='signuppassword' placeholder='Password'/>
-                    <button id='signupbutton'>Sign Up</button>
+                    <Link to='/activities'>
+                        <button id='signupbutton'>Sign Up</button>
+                    </Link>
+                    
                 </form>
                 </div>
                 
