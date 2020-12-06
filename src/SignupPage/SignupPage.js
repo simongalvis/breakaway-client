81bef75ad694a97f5c96befbf40ef827a263422c
Simon Galvis <simongalvis9@gmail.com>
2020-12-06 01:43:26 -0700

    Add alert to direct user to login after successful account creation

--- src/SignupPage/SignupPage.js
+++ src/SignupPage/SignupPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../SignupPage/SignupPage.css';
 import NavBar from '../NavBar/NavBar.js';
-import { Link } from 'react-router-dom';
+import ApiContext from '../ApiContext';
 
 
 
@@ -9,7 +9,30 @@ import { Link } from 'react-router-dom';
 
 
 class SignupPage extends React.Component{
+static contextType = ApiContext;
 
+    handleSubmit = e =>{
+        e.preventDefault();
+
+       const  { signupFullname, signupUsername, signupPassword } = e.target;
+
+       //console.log(` signupFullname: ` +signupFullname.value +  `signupUsername: ` + signupUsername.value + `signupPassword: ` + signupPassword.value)
+
+
+       const user = {
+           fullname: signupFullname.value,
+           username: signupUsername.value,
+           password: signupPassword.value
+       }
+       this.context.createUser(user)
+       this.context.refreshDatabase()
+       alert('Account successfully created. Now Click the login button on the top right of the page to login and get started!')
+
+       
+    }
+
+     
+    
     
     render(){
         return(
@@ -17,12 +40,12 @@ class SignupPage extends React.Component{
                 <NavBar/>
                 <div className='signupFormContainer'>
                 <h2 id='signupheading'>Become part of the Breakaway crew</h2>
-                <form>
-                    <input  type='text' id='signupusername' name='signupusername' placeholder='Username'/>
-                    <input  type='text' id='signuppassword' name='signuppassword' placeholder='Password'/>
-                    <Link to='/activities'>
-                        <button id='signupbutton'>Sign Up</button>
-                    </Link>
+                <form onSubmit={this.handleSubmit}> 
+                    <input  type='text' id='signupFullname' name='signupFullname' placeholder='Full name' required/>
+                    <input  type='text' id='signupUsername' name='signupUsername' placeholder='Username' required/>
+                    <input  type='text' id='signupPassword' name='signupPassword' placeholder='Password' required/>
+                    <button id='signupbutton'>Sign Up</button>
+                    
                     
                 </form>
                 </div>
