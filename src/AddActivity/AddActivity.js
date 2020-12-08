9c1ff8e2abf5bb41ab91a6be407ad4a27aa94325
Simon Galvis <simongalvis9@gmail.com>
2020-12-07 18:38:39 -0700

    Add labels to AddActivity form inputs

--- src/AddActivity/AddActivity.js
+++ src/AddActivity/AddActivity.js
@@ -42,9 +42,13 @@ static contextType = ApiContext;
                 <div className='loginFormContainer'>
                 <h2 id='loginheading'>Add Activity</h2>
                 <form onSubmit={this.handleSubmit}>
+                    <label for='titleInput'><b>Title:</b></label>
                     <input  type='text' id='titleInput' name='titleInput' placeholder='Title' />
+                    <label for='durationInput'><b>Duration:</b> </label>
                     <input  type='number' id='durationInput' name='durationInput' placeholder='Duration(minutes)' required/>
+                    <label for='distanceInput'><b>Distance:</b> </label>
                     <input  type='number' id='distanceInput' name='distanceInput' placeholder='Distance(miles)' required/>
+                    <label for='descriptionInput'><b>Notes:</b> </label>
                     <input  type='text' id='descriptionInput' name='descriptionInput' placeholder='Notes(Ex: Felt great after the first mile)' required/>
                     <button type='submit' id='activity-submit-button'>Submit</button>
                    
