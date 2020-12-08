116712564a313caeac827764f3c91b7ad3fa62f3
Simon Galvis <simongalvis9@gmail.com>
2020-12-07 19:12:22 -0700

    Add conditional route for main home button depending on whether or not there is a logged user

--- src/DashboardNavBar/DashboardNavBar.js
+++ src/DashboardNavBar/DashboardNavBar.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './DashboardNavBar.css';
 import logo from '../images/breakaway-logo.png'
+import ApiContext from '../ApiContext';
 
 
 
 class DashboardNavBar extends React.Component{
-
+static contextType = ApiContext;
 
 
     render(){
         return(
             <header>
-                <Link to='/'>
+                <Link to={Object.keys(this.context.loggedUser).length > 0 ? '/activities' : '/'}>
                     <img src={logo} alt='Blue breakaway logo' id='main-logo'/>
                 </Link>
                 <Link to="/new-activity">
