2e32c8965a3e9b8a92031a074bc55742f4d6e7bf
Simon Galvis <simongalvis9@gmail.com>
2020-12-07 21:49:59 -0700

    Add app description on landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -12,7 +12,8 @@ class LandingPage extends React.Component{
             
             <div className='LandingPage'>
                 
-                <h2 className='hero'>The #1 way to track your athletic progress</h2>
+                <h2 className='hero'>The simple way to track your athletic progress</h2>
+                <p id='hero-text'>Breakaway allows you to log your runs, walks, hikes, and other activities in one convenient location</p>
                 <img src={sampleHero} id="landingpage-ui-image" alt="Breakaway ui on mobile"/>
                 <Link to='/signup-page'>
                     <button id='landingpage-signup-button'>Sign up</button>
